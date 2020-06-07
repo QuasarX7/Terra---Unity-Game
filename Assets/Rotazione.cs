3c079dddfb5e2e394dfb81453dc7fe978fa746bd
Domenico della PERUTA <quasar_x7@yahoo.it>
2020-06-07 18:14:34 +0200

    x

---
+++ Assets/Rotazione.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotazione : MonoBehaviour
+{
+    private const int PULSANTE_MOUSE_SX = 0;
+    private const int PULSANTE_MOUSE_DX = 1;
+    private const int PULSANTE_MOUSE_CX = 2;
+
+    private static Vector3 ZERO = new Vector3(0,0,0);
+
+    private bool trascinamentoMouse = false;
+    private Vector3 inizioPosizioneMouse = ZERO, finePosizioneMouse= ZERO;
+    private Vector3 spostramentoMouse = ZERO;
+
+    [SerializeField] float velocita = 10.0f;// velocità naturale di rotazione
+
+    void Start(){
+    }
+    
+
+    void Update(){
+        // rotazione naturale del pianeta Terra
+        transform.Rotate(0, velocita*Time.deltaTime, 0, Space.Self);
+
+        // intercetta eventi
+        bool mousePremuto = Input.GetMouseButtonDown(PULSANTE_MOUSE_SX) || 
+                            Input.GetMouseButtonDown(PULSANTE_MOUSE_DX) || 
+                            Input.GetMouseButtonDown(PULSANTE_MOUSE_CX);
+        
+
+        bool mouseRilasciato =  Input.GetMouseButtonUp(PULSANTE_MOUSE_SX) || 
+                                Input.GetMouseButtonUp(PULSANTE_MOUSE_DX) || 
+                                Input.GetMouseButtonUp(PULSANTE_MOUSE_CX);
+
+
+        if(mousePremuto && !this.trascinamentoMouse){ // inizio trascinamento
+            this.inizioPosizioneMouse = Input.mousePosition;
+            this.trascinamentoMouse = true;
+        
+        }else if(mouseRilasciato && this.trascinamentoMouse){ // fine trascinamento
+            this.finePosizioneMouse = Input.mousePosition;
+            this.trascinamentoMouse = false;
+        }
+        
+        if(this.inizioPosizioneMouse !=ZERO && this.finePosizioneMouse != ZERO){
+            // calcola spostamento
+            this.spostramentoMouse = this.finePosizioneMouse - this.inizioPosizioneMouse;
+            transform.Rotate(this.spostramentoMouse.x*0.1f, 0, this.spostramentoMouse.y*0.1f, Space.Self);
+
+            //resetta vettori
+            this.inizioPosizioneMouse = this.finePosizioneMouse = ZERO;
+        }
+        
+        //Debug.Log(Input.mousePosition);
+        
+    }
+}
